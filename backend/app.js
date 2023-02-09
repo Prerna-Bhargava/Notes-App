04392ee63ebdf6e689e70e672b75d0ef0d4f38d6
Prerna-Bhargava <prerna172002@gmail.com>
2023-02-09 22:39:34 +0530

    optimised

--- backend/app.js
+++ backend/app.js
@@ -18,26 +18,22 @@ app.use('/api/notes', noteRoutes)
 // ----deployment----
 
 __dirname = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "/frontend/build")));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//     })
-// }
-// else {
-//     app.get("/", (req, res) => {
-//         res.send("API is running..")
-//     })
-// }
-
-app.get("/", (req, res) => {
-    res.send("API is running..")
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    })
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("API is running..")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
 
-const port = process.env.PORT
+const port = process.env.PORT || 9000
 app.listen(port, () => {
     console.log("Hey App has started Listening")
 })
